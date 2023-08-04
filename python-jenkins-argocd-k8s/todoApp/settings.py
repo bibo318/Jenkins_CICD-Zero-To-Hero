90aec425775da696063e6e842d25af7f024a6de8
DemonGod <mr.dndung@gmail.com>
2023-08-04 09:47:23 +0700

    Update settings.py

--- python-jenkins-argocd-k8s/todoApp/settings.py
+++ python-jenkins-argocd-k8s/todoApp/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/data/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
